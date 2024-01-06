100c666333c0c54427d2ac82b62f83cdce89cd65
tonildo123 <tony81191@gmail.com>
2024-01-06 20:30:23 -0300

    redux login OK

--- src/hooks/useRegisterFirebase.tsx
+++ src/hooks/useRegisterFirebase.tsx
@@ -6,16 +6,18 @@ import { db, firebaseConfig } from '../firebase/index'
 import { collection, addDoc, query, where, getDocs } from "firebase/firestore";
 
 import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { RootStackParamsList } from '../../App';
 import { Alert } from 'react-native';
+import { useDispatch } from 'react-redux';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { setUserInfo } from '../state/ProfileSlice';
 
 
 const useRegisterFirebase = () => {
 
     const app = initializeApp(firebaseConfig);
     const auth = getAuth(app)
+
+    const distpach = useDispatch()
 
     const navigation = useNavigation();
     const [error, setError] = useState("");
@@ -117,16 +119,23 @@ const useRegisterFirebase = () => {
                         // SI EXISTE EN FIRESTORE LO MUESTRA
                         if (selectedProfile) {
 
-                            // const user = {
-                            //     id: selectedProfile.user_id,
-                            //     email: selectedProfile.email,
-                            //     role: selectedProfile.role
-                            // }
+                            const user = {
+                                id: selectedProfile.user_id,
+                                email: selectedProfile.email,
+                                role: selectedProfile.role,
+                                urlphoto:selectedProfile.urlphoto,
+                                document:selectedProfile.document,
+                                name:selectedProfile.name,
+                                lastname:selectedProfile.lastname,
+                                phone:selectedProfile.phone,
+                                birthdate:selectedProfile.birthdate,
+                                logged:true
+                            }
+                            console.log('mi user es ', JSON.stringify(selectedProfile, null, 6))
                             // localStorage.setItem("zxcasd", email);
                             // localStorage.setItem("qwefgh", password);
-                            // distpach(loggearme(user))
-                            console.log(JSON.stringify(selectedProfile, null, 6))
-                            navigation.navigate("Home")
+                            distpach(setUserInfo(user))
+                            
 
                         } else {
                             console.log('usuario no existe .', selectedProfile)
