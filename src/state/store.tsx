2eee370402568e3ec4fcea0ab96802b277866844
tonildo123 <tony81191@gmail.com>
2024-01-06 21:03:27 -0300

    Redux OK

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import ProfileSlice from './ProfileSlice'
+import CalendarySlice from './CalendarySlice'
 
 
 export const store = configureStore({
   reducer: {
     user:ProfileSlice,
+    calendary: CalendarySlice
   },
 })
