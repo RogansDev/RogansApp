69bc8c067378437955e4e21b8e47d72fe7f39069
tonildo123 <tony81191@gmail.com>
2024-01-09 00:09:09 -0300

    se corrijio bug

--- src/Presentation/components/SingLogin.tsx
+++ src/Presentation/components/SingLogin.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet} from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { MyColors, MyFont } from '../theme/AppTheme';
 import Icons from '../theme/Icons';
 import { useNavigation } from '@react-navigation/native';
@@ -7,51 +7,46 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import { RootParamList } from '../../utils/RootParamList';
 
 interface Props {
-    text: string,
-    onPress: () => void;
+  text: string,
+  onPress: () => void;
 }
+
 const SingLogin: React.FC<Props> = ({ text, onPress }) => {
-
   const navigation = useNavigation<StackNavigationProp<RootParamList>>();
-
   const { SendIcon } = Icons;
 
-
   return (
-    <TouchableOpacity style={styles.roundedBottom} onPress={() => navigation.navigate("PassUpdatekeyDash")}>
+    <TouchableOpacity style={styles.roundedBottom} onPress={onPress}>
       <View style={styles.flexBttom}>
         <Text style={styles.textBottom}>{text}</Text>
         <SendIcon width={20} height={20} />
       </View>
     </TouchableOpacity>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
-    roundedBottom: {
-        width: "100%",
-        height: 45,
-        display: 'flex',
-        alignSelf: 'center',
-        backgroundColor: MyColors.black,
-        justifyContent: 'center',
-        borderRadius: 15,
-    }, 
-    flexBttom: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 10,
-    },
-    textBottom: {
-        color: 'white',
-        fontSize: 15,
-        fontFamily: MyFont.regular,
-    },
-    logoLogin: {
-        width: 20,
-        height: 20,
-    }
-})
+  roundedBottom: {
+    width: '100%',
+    height: 45,
+    display: 'flex',
+    alignSelf: 'center',
+    backgroundColor: MyColors.black,
+    justifyContent: 'center',
+    borderRadius: 15,
+  },
+  flexBttom: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
+  },
+  textBottom: {
+    color: 'white',
+    fontSize: 15,
+    fontFamily: MyFont.regular,
+  },
+});
+
 export default SingLogin;
