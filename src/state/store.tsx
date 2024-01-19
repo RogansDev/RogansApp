2660faaeecd3eeb2e93d7e49c6c68bb240e65175
tonildo123 <tony81191@gmail.com>
2024-01-19 12:22:08 -0300

    promotion slice OK

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import ProfileSlice from './ProfileSlice'
 import CalendarySlice from './CalendarySlice'
 import CodeSlice from './CodeSlice'
+import PromotionSlice from './PromotionSlice'
 
 
 export const store = configureStore({
   reducer: {
     user:ProfileSlice,
     calendary: CalendarySlice,
-    code: CodeSlice
+    code: CodeSlice,
+    promotions: PromotionSlice
   },
 })
