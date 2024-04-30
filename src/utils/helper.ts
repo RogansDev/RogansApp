faf8228060136c37bfac41dff0e9945f0b3e902b
tonildo123 <tony81191@gmail.com>
2024-04-29 21:42:32 -0300

    mercado pago OK

---
+++ src/utils/helper.ts
@@ -0,0 +1,9 @@
+export function obtenerFechaActual(): string {
+    const fecha = new Date();
+    const dia = fecha.getDate().toString().padStart(2, '0');
+    const mes = (fecha.getMonth() + 1).toString().padStart(2, '0');
+    const año = fecha.getFullYear().toString();
+    return `${dia}/${mes}/${año}`;
+}
+
+
