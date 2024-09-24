54fa98036a7506587fa2a08c2af3e2ded2d4b9ac
RogansDev <desarrollo.rogansya@gmail.com>
2024-09-23 20:21:47 -0500

    Commit de prueba

---
+++ ios/Pods/Target Support Files/Pods-RogansApp/Pods-RogansApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RogansApp : NSObject
+@end
+@implementation PodsDummy_Pods_RogansApp
+@end
---
+++ ios/Pods/Target Support Files/react-native-clarity/react-native-clarity-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "Clarity.h"
+
+FOUNDATION_EXPORT double react_native_clarityVersionNumber;
+FOUNDATION_EXPORT const unsigned char react_native_clarityVersionString[];
+
